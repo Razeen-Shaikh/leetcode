3fe661ff4e61b54008080f2919db0f621bb069fb
Razeen Shaikh <razeen9796@outlook.com>
2022-07-02 22:48:30 +0530

    leetcode 1480: solution in python

---
+++ 1480.running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -0,0 +1,9 @@
+from ast import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sums = []
+        for i in range(len(nums)):
+            sums.append(sum(nums[0:i+1]))
+        return sums
