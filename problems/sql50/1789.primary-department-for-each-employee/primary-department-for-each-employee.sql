3eab73adfcd0db1e1ba87d8666fc0f5eb3f36093
Razeen Shaikh <34880700+Razeen-Shaikh@users.noreply.github.com>
2024-02-10 01:05:35 +0530

    primary department for each employee

---
+++ problems/sql50/1789.primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,12 @@
+SELECT
+    employee_id,
+    CASE
+        WHEN COUNT(*) = 1 THEN MAX(department_id)
+        ELSE MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
+    END AS department_id
+FROM
+    Employee
+GROUP BY
+    employee_id
+HAVING
+    department_id IS NOT NULL;
