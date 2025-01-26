ff65745012538e680bcd19978939ed8ce3a477d5
Razeen-Shaikh <razeen9796@outlook.com>
2025-01-26 16:19:35 +0530

    longest substring

--- problems/0003.longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.py
+++ problems/0003.longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.py
@@ -9,20 +9,16 @@ class Solution:
         Returns:
             int: The length of the longest substring without repeating characters.
         """
-        n = len(s)
+        char_set = set()
         max_length = 0
         start = 0
-        char_index_map = {}
 
-        for end in range(n):
-            # If the current character is already in the map
-            # and its index is after the start of the current substring,
-            # update the start of the substring to the next index of the repeated character.
-            if s[end] in char_index_map and char_index_map[s[end]] >= start:
-                start = char_index_map[s[end]] + 1
-            # Update the index of the current character in the map.
-            char_index_map[s[end]] = end
-            # Update the maximum length if needed.
+        for end in range(len(s)):
+            while s[end] in char_set:
+                char_set.remove(s[start])
+                start += 1
+            
+            char_set.add(s[end])
             max_length = max(max_length, end - start + 1)
-
+        
         return max_length
