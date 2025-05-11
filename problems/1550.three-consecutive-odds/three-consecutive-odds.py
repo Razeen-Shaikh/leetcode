d1dfb6215a46dd2723d8deb6f8c4044d77474cba
Razeen Shaikh <34880700+Razeen-Shaikh@users.noreply.github.com>
2025-05-11 23:05:03 +0530

    Update problems/1550.three-consecutive-odds/three-consecutive-odds.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- problems/1550.three-consecutive-odds/three-consecutive-odds.py
+++ problems/1550.three-consecutive-odds/three-consecutive-odds.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        for i in range(len(arr) - 2):
-            if arr[i] % 2 == 1 and arr[i+1] % 2 == 1 and arr[i+2] % 2 == 1:
-                return True
-        return False
+        return any(
+            arr[i] % 2 == 1 and arr[i + 1] % 2 == 1 and arr[i + 2] % 2 == 1
+            for i in range(len(arr) - 2)
+        )
