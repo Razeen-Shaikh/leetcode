abe44c6287398169f1f0b038693aeafeaacca9a8
Razeen Shaikh <razeen9796@outlook.com>
2025-05-20 15:50:31 +0000

    added some more problems solution

---
+++ problems/3024.type-of-triangle/type-of-triangle.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public string TriangleType(int[] nums)
+    {
+        if (nums[0] + nums[1] <= nums[2] || nums[1] + nums[2] <= nums[0] || nums[2] + nums[0] <= nums[1])
+        {
+            return "none";
+        }
+
+        if (nums[0] == nums[1] && nums[1] == nums[2])
+        {
+            return "equilateral";
+        }
+
+        if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2])
+        {
+            return "isosceles";
+        }
+
+        return "scalene";
+    }
+}
