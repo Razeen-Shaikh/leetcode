f17230daea192b421e812fd5b3f4760c33ab70d6
Razeen Shaikh <razeen9796@outlook.com>
2025-05-23 13:05:06 +0000

    Added Swap Nodes In Pairs

---
+++ problems/0061.rotate-list/RotateList.cs
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode RotateRight(ListNode head, int k)
+    {
+        if (head == null || head.next == null || k == 0) return head;
+
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null)
+        {
+            tail = tail.next;
+            length++;
+        }
+
+        tail.next = head;
+
+        k = k % length;
+        int stepsToNewTail = length - k;
+        ListNode newTail = head;
+        for (int i = 1; i < stepsToNewTail; i++)
+        {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
