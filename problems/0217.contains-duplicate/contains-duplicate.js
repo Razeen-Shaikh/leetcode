2a7de11e1b82a62efbba1ba7860cadc521644633
Razeen Shaikh <razeen9796@outlook.com>
2025-07-11 06:17:08 +0000

    Added multiple solutions for contain duplicates

--- problems/0217.contains-duplicate/contains-duplicate.js
+++
@@ -1,8 +0,0 @@
-/**
- * 217. Contains Duplicate
- * @param {number[]} numb
- * @return {boolean}
- */
-var containsDuplicate = function (numb) {
-	return numb.some((num, index) => numb.indexOf(num) !== index);
-};
---
+++ problems/0217.contains-duplicate/set-size-check.js
@@ -0,0 +1,8 @@
+/**
+ * 217. Contains Duplicate
+ * @param {number[]} numb
+ * @return {boolean}
+ */
+const containsDuplicate = function (numb) {
+  return new Set(nums).size !== nums.length;
+};
