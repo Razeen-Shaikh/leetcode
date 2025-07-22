750cc2ab66817f3cab45a8462d8f0be3525bd70b
Razeen Shaikh <razeen9796@outlook.com>
2025-07-22 12:20:27 +0000

    Added another solution for Valid Sudoku

---
+++ problems/0036.valid-sudoku/sets-method.js
@@ -0,0 +1,61 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+const isValidSudoku = function (board) {
+  const rows = Array.from({ length: 9 }, () => new Set());
+  const cols = Array.from({ length: 9 }, () => new Set());
+  const boxes = Array.from({ length: 9 }, () => new Set());
+
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      const value = board[r][c];
+      if (value === ".") {
+        continue;
+      }
+
+      const boxIndex = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+      console.log({ r, c, boxIndex });
+
+      if (
+        rows[r].has(value) ||
+        cols[c].has(value) ||
+        boxes[boxIndex].has(value)
+      ) {
+        return false;
+      }
+
+      rows[r].add(value);
+      cols[c].add(value);
+      boxes[boxIndex].add(value);
+
+      console.log( {rows:rows[r], cols:cols[c], boxes:boxes[boxIndex]} );
+    }
+  }
+
+  return true;
+};
+
+isValidSudoku([
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]);
+
+isValidSudoku([
+  ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]);
