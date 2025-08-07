659ad73fa392f74835251d5f116c7e75760805c2
Razeen Shaikh <razeen9796@outlook.com>
2025-08-07 05:41:13 +0000

    Trapping Rain Water

---
+++ problems/0042.trapping-rain-water/two-pointer.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const trap = function (height) {
+  let left = 0,
+    right = height.length - 1;
+  let maxLeft = 0,
+    maxRight = 0;
+  let water = 0;
+
+  while (left < right) {
+    if (height[left] < height[right]) {
+      if (height[left] >= maxLeft) {
+        maxLeft = height[left];
+      } else {
+        water += maxLeft - height[left];
+      }
+      left++;
+    } else {
+      if (height[right] >= maxRight) {
+        maxRight = height[right];
+      } else {
+        water += maxRight - height[right];
+      }
+      right--;
+    }
+  }
+
+  return water;
+};
