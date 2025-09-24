a419d0f6770c14fde2006454672d73d9e78efda2
Razeen Shaikh <34880700+Razeen-Shaikh@users.noreply.github.com>
2025-09-24 09:56:19 +0530

    Update problems/0020.valid-parentheses/valid-parentheses.js
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- problems/0020.valid-parentheses/valid-parentheses.js
+++ problems/0020.valid-parentheses/valid-parentheses.js
@@ -13,11 +13,10 @@ var isValid = function(s) {
     for (let char of s) {
         if (Object.values(bracketMap).includes(char)) {
             stack.push(char);
-        } else if (Object.keys(bracketMap).includes(char)) {
-            if (stack.pop() !== bracketMap[char]) {
-                return false;
-            }
-        }
+        } else if (Object.keys(bracketMap).includes(char) && stack.pop() !== bracketMap[char]) {
+                     return false;
+               }
+
     }
 
     return stack.length === 0;
