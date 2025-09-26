34639a95847365e00f73f50fb49c00c15d285627
Razeen Shaikh <razeen9796@outlook.com>
2025-09-26 10:38:02 +0530

    Solved Evaluate Reverse Polish Notation

---
+++ problems/0150.evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  const stack = [];
+
+  for (const token of tokens) {
+    if (["+", "-", "*", "/"].includes(token)) {
+      const b = stack.pop();
+      const a = stack.pop();
+      let result;
+
+      switch (token) {
+        case "+":
+          result = a + b;
+          break;
+        case "-":
+          result = a - b;
+          break;
+        case "*":
+          result = a * b;
+          break;
+        case "/":
+          result = Math.trunc(a / b);
+          break;
+      }
+
+      stack.push(result);
+    } else {
+      stack.push(Number(token));
+    }
+  }
+
+  return stack[0];
+};
