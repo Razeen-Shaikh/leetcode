5b4faeec0d463d7e2058287276ad14750f250679
Razeen Shaikh <razeen9796@outlook.com>
2025-10-10 15:32:25 +0530

    Time Based Key Value Store

---
+++ problems/0981.time-based-key-value-store/time-based-key-value-store.js
@@ -0,0 +1,44 @@
+var TimeMap = function () {
+  this.store = new Map();
+};
+
+/**
+ * @param {string} key
+ * @param {string} value
+ * @param {number} timestamp
+ * @return {void}
+ */
+TimeMap.prototype.set = function (key, value, timestamp) {
+  if (!this.store.has(key)) {
+    this.store.set(key, []);
+  }
+  this.store.get(key).push([timestamp, value]);
+};
+
+/**
+ * @param {string} key
+ * @param {number} timestamp
+ * @return {string}
+ */
+TimeMap.prototype.get = function (key, timestamp) {
+  if (!this.store.has(key)) return "";
+
+  const entries = this.store.get(key);
+  let left = 0,
+    right = entries.length - 1;
+  let result = "";
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const [ts, val] = entries[mid];
+
+    if (ts <= timestamp) {
+      result = val;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return result;
+};
