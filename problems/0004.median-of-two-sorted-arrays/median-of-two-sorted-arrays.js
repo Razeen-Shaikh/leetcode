a1493474d0c51cfa51c6a527b5bd4f902bee28aa
Razeen Shaikh <razeen9796@outlook.com>
2025-10-10 15:46:03 +0530

    Updated README.md

--- problems/0004.median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ problems/0004.median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -4,18 +4,18 @@
  * @param {number[]} numb2
  * @return {number}
  */
-var findMedianSortedArrays = function (numb1, numb2) {
-	var merged = numb1.concat(numb2).sort((a, b) => a - b);
-	var median = 0;
+let findMedianSortedArrays = function (numb1, numb2) {
+  let merged = numb1.concat(numb2).sort((a, b) => a - b);
+  let median = 0;
 
-	if (merged.length % 2 === 0) {
-		median =
-			(merged[parseInt(merged.length / 2) - 1] +
-				merged[parseInt(merged.length / 2)]) /
-			2;
-	} else {
-		median = merged[parseInt(merged.length / 2)];
-	}
+  if (merged.length % 2 === 0) {
+    median =
+      (merged[parseInt(merged.length / 2) - 1] +
+        merged[parseInt(merged.length / 2)]) /
+      2;
+  } else {
+    median = merged[parseInt(merged.length / 2)];
+  }
 
-	return median;
+  return median;
 };
