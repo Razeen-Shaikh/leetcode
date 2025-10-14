684a2de77760a7a0d36f464ca6341bd070de17c0
Razeen Shaikh <razeen9796@outlook.com>
2025-10-14 11:41:45 +0530

    Solve Reorder List

---
+++ problems/0143.reorder-list/reorder-list.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+let reorderList = function (head) {
+  if (!head || !head.next) return;
+
+  let slow = head,
+    fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  let prev = null,
+    curr = slow;
+
+  while (curr) {
+    let nextTemp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextTemp;
+  }
+
+  let first = head,
+    second = prev;
+
+  while (second.next) {
+    let temp1 = first.next;
+    let temp2 = second.next;
+
+    first.next = second;
+    second.next = temp1;
+
+    first = temp1;
+    second = temp2;
+  }
+};
